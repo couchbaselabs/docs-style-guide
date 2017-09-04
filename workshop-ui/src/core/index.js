cdaf09d0fe66139e5dc035aea3bf6f13586d67d8
James Nocentini <james.nocentini@gmail.com>
2017-09-05 00:28:26 +0200

    render milestones and content.

--- workshop-ui/src/core/index.js
+++ workshop-ui/src/core/index.js
@@ -2,10 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import marked from "marked"
 import "whatwg-fetch"
+import jsyaml from "js-yaml"
 
 module.exports = function ConfigUI(opts) {
   console.log(opts.specs);
-  ReactDOM.render(<StandaloneLayout specs={opts.specs} />, document.getElementById('swagger-ui'));
+  ReactDOM.render(<StandaloneLayout url={opts.url} />, document.getElementById('swagger-ui'));
 };
 
 export default class StandaloneLayout extends React.Component {
@@ -13,42 +14,92 @@ export default class StandaloneLayout extends React.Component {
   constructor() {
     super();
     this.state = {
-      specs: []
+      spec: null,
+      selectedState: null,
+      currentMilestone: null
     };
   }
   
   componentWillMount() {
+    fetch(this.props.url)
+      .then(res => {
+        return res.text()
+      })
+      .then(function(res) {
+        let spec = jsyaml.load(res);
+        this.setState({spec: spec, selectedLesson: 0, currentMilestone: 0});
+      }.bind(this));
   }
+  
+  getLessonNames() {
+    return this.state.spec.lessons.map(lesson => {
+      return lesson.title;
+    });
+  }
+  
+  getMilestoneNames(selectedLesson) {
+    console.log(this.state.spec.lessons[selectedLesson]);
+    return this.state.spec.lessons[selectedLesson].milestones;
+  }
+  
+  
 
   render() {
-    
     let markdownString = '```json\n ' + JSON.stringify(this.state.specs, null, 2) + '\n```';
     let renderer = new marked.Renderer();
     renderer.code = function(code, lang, escaped) {
       
     };
     
-    console.log(marked(markdownString));
-    
-    return (
-      <div className="docs-ui">
-        <div className="drawer">
-          <div className="left-nav">
-            <a className="toc-item" href="javascript:;">
-              <i>1</i>
-              <span>Query</span>
-            </a>
-          </div>
-        </div>
-        <div className="opblock-tag no-desc">
-          <div className="opblock">
-            <div className="opblock-summary">
-              <span className="opblock-summary-method">POST</span>
+      if (!this.state.spec) {
+        return <div></div>
+      } else {
+        return (
+          <div className="docs-ui">
+            <div className="drawer">
+              <div className="left-nav">
+                {this.getLessonNames().map((name, index) => {
+                  return (
+                    <a
+                      key={index}
+                      className="toc-item"
+                      href="javascript:;"
+                      onClick={(e) => {this.setState({selectedLesson: index})}}>
+                      <i>{index + 1}</i>
+                      <span>{name}</span>
+                    </a>
+                  )
+                })}
+              </div>
+            </div>
+            <div className="body">
+              <div className="main">
+                <nav className="milestones">
+                  <ol>
+                    {this.getMilestoneNames(this.state.selectedLesson).map((name, index) => {
+                      return (
+                        <li key={index}>
+                          {name.title}
+                        </li>
+                      )
+                    })}
+                  </ol>
+                </nav>
+                <div className="inner">
+                  {this.state.spec.lessons[this.state.selectedLesson].milestones[this.state.currentMilestone].description}  
+                </div>
+              </div>
+            </div>
+            <div className="opblock-tag no-desc">
+              <div className="opblock">
+                <div className="opblock-summary">
+                  <span className="opblock-summary-method">POST</span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    )
+        )
+      }
   }
 
 }
