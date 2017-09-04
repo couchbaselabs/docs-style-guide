030b23efc621c5ce9dd8dff193940445024e2f3c
James Nocentini <james.nocentini@gmail.com>
2017-09-04 12:29:19 +0200

    make all keys clickable.

--- json-config-ui/src/core/index.js
+++ json-config-ui/src/core/index.js
@@ -7,25 +7,73 @@ module.exports = function ConfigUI(opts) {
   ReactDOM.render(<StandaloneLayout specs={opts.specs} />, document.getElementById('swagger-ui'));
 };
 
-let renderer = new marked.Renderer();
-var codeTemplate = renderer.code;
-renderer.code = function(code, lang, escaped) {
-  let rendered = codeTemplate.call(this, code, lang, escaped);
-  rendered = rendered.replace("adminInterface", "<a href=\"\">adminInterface</a>");
-  return rendered;
-};
-
 export default class StandaloneLayout extends React.Component {
   
   constructor() {
     super();
     this.state = {
       specs: [],
-      selected: false
+      selected: false,
+      renderer: new marked.Renderer()
     };
+
+    let codeTemplate = this.state.renderer.code;
+    var that = this;
+    this.state.renderer.code = function(code, lang, escaped) {
+      let rendered = codeTemplate.call(this, code, lang, escaped);
+      let output = replaceStringsWithAnchors(rendered, that.state.specs[that.state.selected]);
+      output.map((element) => {
+        console.log(element)
+        rendered = rendered.replace(element.name, "<a href=\"\">" + element.name + "</a>");
+      });
+      return rendered;
+    };
+
+    function replaceStringsWithAnchors(string, spec) {
+      return mapKeysToPaths(spec.json.properties, null, []);
+      // console.log(string)
+      // Object.keys(string).map((key, index) => {
+      //   console.log(key)
+      // });
+    };
+
+    let mapKeysToPaths = function mapKeysToPaths(props, context, output) {
+      Object.keys(props).map((key, index) => {
+        const type = props[key].type;
+        switch (type) {
+          case 'string':
+            output.push({name: key, path: context + '-' + key});
+            break;
+          case 'integer':
+            output.push({name: key, path: context + '-' + key});
+            break;
+          case 'boolean':
+            output.push({name: key, path: context + '-' + key});
+            break;
+          case 'array':
+            let item_type = props[key].items.type;
+            if (item_type == 'object') {
+              output.push({name: key, path: context + '-' + key});
+              mapKeysToPaths(props[key].items.properties, context + '-' + key, output);
+            } else {
+              output.push({name: key, path: context + '-' + key});
+            }
+            break;
+          case 'object':
+            output.push({name: key, path: context + '-' + key});
+            mapKeysToPaths(props[key].properties, context + '-' + key, output);
+            break;
+          default:
+            break;
+        }
+      });
+      return output;
+    }
   }
   
-  componentWillMount() {
+  
+  
+  componentDidMount() {
     this.props.specs.map(function(spec) {
       fetch(spec.url)
         .then(res => res.json())
@@ -37,6 +85,10 @@ export default class StandaloneLayout extends React.Component {
           });
         }.bind(this));
     }.bind(this));
+    
+    setTimeout(function() {
+      this.selectedVersionChange({target: {value: 1}})
+    }.bind(this), 1000);
   }
 
   mapPropsToJSON(props, initial) {
@@ -80,7 +132,7 @@ export default class StandaloneLayout extends React.Component {
       return '';
     } else {
       let markdownString = '```json\n ' + JSON.stringify(this.mapPropsToJSON(this.state.specs[this.state.selected].json.properties, {}), null, 2) + '\n```';
-      return marked(markdownString, {renderer: renderer});
+      return marked(markdownString, {renderer: this.state.renderer});
     }
   }
 
