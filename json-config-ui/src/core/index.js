84efea11229e6506dafabe34b044b93a4ce7a981
James Nocentini <james.nocentini@gmail.com>
2017-09-07 01:20:39 +0200

    remove hash link.

--- json-config-ui/src/core/index.js
+++ json-config-ui/src/core/index.js
@@ -31,10 +31,6 @@ export default class StandaloneLayout extends React.Component {
 
     function replaceStringsWithAnchors(string, spec) {
       return that.mapKeysToPaths(spec.json.properties, null, []);
-      // console.log(string)
-      // Object.keys(string).map((key, index) => {
-      //   console.log(key)
-      // });
     };
 
   }
@@ -218,12 +214,7 @@ export default class StandaloneLayout extends React.Component {
                   <a href={'#' + row.path} id={row.path}></a>
                   <span className="text">
                     <code>{row.path.split('-').join('.')}</code>
-                    </span>
-                  <svg ariaHidden="true" className="octicon octicon-link" height="20" version="1.1" viewBox="0 -3 20 20"
-                       width="20">
-                    <path
-                      d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-                  </svg>
+                  </span>
                 </h2>
                 <p>
                   <code>{row.type}</code>
@@ -245,22 +236,6 @@ export default class StandaloneLayout extends React.Component {
               </div>
             )
           })}
-          <h3><a href="#ref" id="ref">Ref</a></h3>
-          {this.mapPropsToTableView(this.renderTableView(), [
-            <div>
-              <h4>
-                server configuration
-                <a className="hash-link instructions" id="server" href={'#server'}>
-                  <svg ariaHidden="true" className="octicon octicon-link" height="20" version="1.1" viewBox="0 -3 20 20"
-                       width="20">
-                    <path
-                      d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-                  </svg>
-                </a>
-              </h4>
-              <TableView config={this.renderTableView()}/>
-            </div>
-          ])}
         </div>
       )
     }
