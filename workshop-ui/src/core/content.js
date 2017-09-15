79bdbde640762738becfe60d60bdeda0a3421f3d
James Nocentini <james.nocentini@gmail.com>
2017-09-15 11:25:14 +0200

    highlight selected platform button.

--- workshop-ui/src/core/content.js
+++ workshop-ui/src/core/content.js
@@ -4,10 +4,12 @@ export default class Content extends React.Component {
   
   constructor() {
     super();
-    
   }
   
   render() {
+    if (this.props.platform != this.props.match.params.platform) {
+      this.props.passPlatform(this.props.match.params.platform);
+    }
     return (
       <div className={`inner-content display-platform-${this.props.match.params.platform}`}>
         <div dangerouslySetInnerHTML={{__html: this.props.description}} />
