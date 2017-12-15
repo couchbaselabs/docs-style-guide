fa62422afb16985319270f7d8e600e6c1d857d47
James Nocentini <james.nocentini@gmail.com>
2017-12-15 13:18:57 +0000

    update links

--- workshop-ui/src/core/welcome.js
+++ workshop-ui/src/core/welcome.js
@@ -19,9 +19,9 @@ export default class Welcome extends React.Component {
                   <p className="para-18">We will develop a sample Travel application for mobile platforms using Couchbase Mobile 2.0. The simple app will allow users to search for and make flight reservations and will also allow searches for hotels based on specific criteria.
                     The mobile app will use the Couchbase Lite 2.0 API that includes support for a N1QL like query interface and Full Text Search (FTS) capabilities. The app will sync documents with Sync Gateway 1.5 and Couchbase Server 5.0 using the 2.0 replication protocol. Additionally, we will use a web version of the Travel Sample app to demonstrate seamless data sync between web and mobile apps.
                     Begin by choosing “Swift” for iOS , “Java” for Android version and “C Sharp” for UWP version. The app will support other mobile platforms shortly.</p>
-                  <Link to="/tutorials/travel-sample/develop/swift" className=" hyperlink-effect">Swift</Link><br/><br/>
-                  <Link to="/tutorials/travel-sample/develop/java" className=" hyperlink-effect">Java</Link><br/><br/>
-                  <Link to="/tutorials/travel-sample/develop/csharp" className=" hyperlink-effect">C Sharp</Link>
+                  <Link to="develop/swift" className=" hyperlink-effect">Swift</Link><br/><br/>
+                  <Link to="develop/java" className=" hyperlink-effect">Java</Link><br/><br/>
+                  <Link to="develop/csharp" className=" hyperlink-effect">C Sharp</Link>
                 </div>
               </div>
               <div className="col-two move-left" style={{animationDuration: '1.2s',animationDelay: '0.3s',animationName: 'fadeInLeft',height: '338px'}}>
