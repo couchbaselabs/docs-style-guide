f0b0f230fa4d1151c69e49dcc69957e195e773e1
James Nocentini <james.nocentini@gmail.com>
2017-12-21 17:34:03 +0000

    output to build folder.

--- react/website/lib/server/generate.js
+++ react/website/lib/server/generate.js
@@ -4,9 +4,11 @@ function execute() {
   const fs = require('fs-extra');
   const jsyaml = require('js-yaml');
   const program = require('commander');
+  const path = require('path');
   
   program.option('--input <path>', 'Specify input path')
     .parse(process.argv);
+  const input_path = program.input;
   
   // create the folder path for a file if it does not exist, then write the file.
   function writeFileAndCreateFolder(file, content) {
@@ -15,9 +17,15 @@ function execute() {
     fs.writeFileSync(file, content);
   }
   
-  let content = fs.readFileSync(program.input);
+  let content = fs.readFileSync(input_path);
   let yaml = jsyaml.load(content);
-  console.log(yaml);
+  
+  /* Read every file path and output to the build dir */
+  yaml.items
+    .map(item => {
+      let content = fs.readFileSync(`${path.dirname(program.input)}/${item.description}`);
+      writeFileAndCreateFolder(`../build/${item.description}`, content);
+    });
   
 }
 
