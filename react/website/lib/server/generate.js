f2558c30f2bcdf681e0e2c1a5a6962359589ae2a
James Nocentini <james.nocentini@gmail.com>
2018-01-05 16:14:22 +0100

    add dev mode in generate script.

--- react/website/lib/server/generate.js
+++ react/website/lib/server/generate.js
@@ -14,13 +14,15 @@ function execute() {
   
   
   const DocsLayout = require('../../core/DocsLayout');
-  const Tutorial = require('../../core/Tutorial');
+  const Site = require('../../core/Site');
   
   program.option('--input <path>', 'Specify input path')
     .option('--mode <mode>', 'Specify a mode')
+    .option('--dev <dev>', 'Run in dev mode')
     .parse(process.argv);
   const input_path = program.input;
   const mode = program.mode;
+  const dev = program.dev;
   
   // create the folder path for a file if it does not exist, then write the file.
   function writeFileAndCreateFolder(file, content) {
@@ -50,43 +52,47 @@ function execute() {
   }
   
   function buildTutorials() {
-    let manifest = fs.readFileSync('../../tutorials/manifest.yaml');
-    let yaml = jsyaml.load(manifest);
-    
-    rx.Observable.fromArray(yaml.items)
-      .flatMap(tutorial => {
-        return requestRx.get(tutorial.link, {headers: {'User-Agent': 'request'}})
-      })
-      .flatMap(data => {
-        console.log('hello');
-        let response = JSON.parse(data.body);
-        let content = base64.decode(response.content);
-        const tutorialComp = (
-          <Tutorial
-            content={content}>
-          </Tutorial>
-        );
-        const str = renderToStaticMarkup(tutorialComp);
-        let repo = response.url.split('/')[5];
-        writeFileAndCreateFolder(`../build/${repo}/${response.name.replace('.md', '.html')}`, str);
-        return response.name;
-      })
-      .subscribe(
-        data => {
-          console.log('Wrote' +  data)
-        },
-        error => {
-          throw error;
-        },
-        () => {
-          mkdirp.sync('../build/css');
-          let content = fs.readFileSync('static/css/styles.css');
-          fs.writeFileSync('../build/css/styles.css', content);
-          console.log('completed');
-        }
+    if (dev) {
+      let content = fs.readFileSync(input_path, 'utf8');
+      const tutorialComp = (
+        <Site content={content}/>
       );
-    
-    setTimeout(() => {}, 5000);
+      const str = renderToStaticMarkup(tutorialComp);
+      writeFileAndCreateFolder('../build/swift.html', str);
+    } else {
+      let manifest = fs.readFileSync('../../tutorials/manifest.yaml');
+      let yaml = jsyaml.load(manifest);
+      rx.Observable.fromArray(yaml.items)
+        .flatMap(tutorial => {
+          return requestRx.get(tutorial.link, {headers: {'User-Agent': 'request'}})
+        })
+        .flatMap(data => {
+          let response = JSON.parse(data.body);
+          let content = base64.decode(response.content);
+          const tutorialComp = (
+            <Site content={content}/>
+          );
+          const str = renderToStaticMarkup(tutorialComp);
+          let repo = response.url.split('/')[5];
+          writeFileAndCreateFolder(`../build/${repo}/${response.name.replace('.md', '.html')}`, str);
+          return response.name;
+        })
+        .subscribe(
+          data => {
+            console.log('Wrote' +  data)
+          },
+          error => {
+            throw error;
+          },
+          () => {
+            console.log('Completed');
+          }
+        );
+    }
+    /* Copy stylesheet */
+    mkdirp.sync('../build/css');
+    let content = fs.readFileSync('static/css/styles.css');
+    fs.writeFileSync('../build/css/styles.css', content);
   }
   
   switch(mode) {
