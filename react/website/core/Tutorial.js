a46eee0b6416224ca421a927b12d1ad4745d4f91
James Nocentini <james.nocentini@gmail.com>
2018-01-09 16:55:43 +0100

    scroll to element by id feature.

--- react/website/core/Tutorial.js
+++ react/website/core/Tutorial.js
@@ -14,7 +14,7 @@ class Tutorial extends React.Component {
     renderer.heading = function (text, level) {
       var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
 
-      return '<h' + level + '><a class="anchor instructions" name="' + toSlug(text) + '"></a>' + text + '<a class="hash-link instructions" href="#' + toSlug(text) + '"><svg aria-hidden="true" class="octicon octicon-link" height="20" version="1.1" viewBox="0 -3 20 20" width="20"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a></h' + level + '>';
+      return '<h' + level + '><a class="anchor instructions" name="' + toSlug(text) + '" id="' + toSlug(text) + '"></a>' + text + '<a class="hash-link instructions" href="#' + toSlug(text) + '"><svg aria-hidden="true" class="octicon octicon-link" height="20" version="1.1" viewBox="0 -3 20 20" width="20"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a></h' + level + '>';
     };
     
     let content_html = marked(this.props.content, {renderer: renderer});
@@ -101,6 +101,11 @@ class Tutorial extends React.Component {
                       }
                     }
                   }
+                };
+                
+                window.onload = function() {
+                  var id = window.location.hash;
+                  document.getElementById(id).scrollIntoView();
                 };
               `
             }}/>
