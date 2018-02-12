44ccf14983c1bc0b8499b79940e4196df6bbaa2a
Priya Rajagopal <rajagp@users.noreply.github.com>
2018-02-12 12:38:35 -0500

    Updated content to align with CBM 2.0 beta

--- tutorials/src/core/welcome.js
+++ tutorials/src/core/welcome.js
@@ -16,9 +16,9 @@ export default class Welcome extends React.Component {
               <div className="col-two">
                 <div className="intro-content">
                   <h4>Develop</h4>
-                  <p className="para-18">We will develop a sample Travel application for mobile platforms using Couchbase Mobile 2.0. The simple app will allow users to search for and make flight reservations and will also allow searches for hotels based on specific criteria.
-                    The mobile app will use the Couchbase Lite 2.0 API that includes support for a N1QL like query interface and Full Text Search (FTS) capabilities. The app will sync documents with Sync Gateway 1.5 and Couchbase Server 5.0 using the 2.0 replication protocol. Additionally, we will use a web version of the Travel Sample app to demonstrate seamless data sync between web and mobile apps.
-                    Begin by choosing “Swift” for iOS , “Java” for Android version and “C Sharp” for UWP version. The app will support other mobile platforms shortly.</p>
+                  <p className="para-18">We will do a code walkthrough of a sample Travel application for mobile platforms using Couchbase Mobile v2.0. The simple app will allow users to search for and to make flight reservations. Users will also be able to search for hotels based on specific criteria.
+                    The mobile app will use the Couchbase Lite 2.0 API that includes support for a N1QL like query interface and Full Text Search (FTS) capabilities. The app will sync documents with Sync Gateway v2.0  using a new websockets based replication protocol. Additionally, we will use a web version of the Travel Sample app which includes the ability to make flight reservations. Documents are persisted on Couchbase Server and seamlessly synced over to web and mobile clients . 
+                    Begin by choosing “Swift” for iOS , “Java” for Android version and “C Sharp” for UWP/Xamarin (iOS/Android) versions. </p>
                   <Link to="/develop/swift/" className=" hyperlink-effect">Swift</Link><br/><br/>
                   <Link to="/develop/java/" className=" hyperlink-effect">Java</Link><br/><br/>
                   <Link to="/develop/csharp/" className=" hyperlink-effect">C Sharp</Link>
