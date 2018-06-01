62aace86e9330ce07851df6e2f4d4d2dd4ff6003
James Nocentini <james.nocentini@gmail.com>
2018-06-01 11:30:01 +0100

    Changes:
    - update README with instructions
    - generate rel notes for each filter id
    - add instructions to set filter permissions
    - add fs-extra module to move the ./img folder to the build output

--- release-notes-generator/server.js
+++ release-notes-generator/server.js
@@ -1,71 +1,84 @@
 import requestRx from './requestRx';
 import rx from 'rx';
 import Mustache from 'mustache';
-import fs from 'fs';
+const fs = require('fs-extra');
 
 let username = process.env.JIRA_USERNAME
   , password = process.env.JIRA_PASSWORD
   , url = `https://${username}:${password}@issues.couchbase.com`
   , post_body = {json: true};
 
-let known_issues = 'project = "Couchbase Server" AND fixVersion in (Vulcan, Alice, Mad-Hatter) AND labels = releasenote AND (level is EMPTY OR level not in (Private,Playdom,Zynga) )'
-  , options = Object.assign({
-      body: {
-        "jql": known_issues,
-        "fields": ["summary", "components", "comment"] /* Fields we need for release notes */
-      },
-      url: `${url}/rest/api/2/search?expand=renderedBody`
-    }, post_body);
+let filter_ids = [16001, 15973];
 
-let result = {issues: []};
-requestRx.post(options)
-  .flatMap(result => {
-    return rx.Observable.fromArray(result.body.issues);
-  })
-  .flatMap(issue => {
-    return rx.Observable.zip(
-      rx.Observable.fromArray([issue]),
-      requestRx.get(`${url}/rest/api/2/issue/${issue.key}/comment?expand=renderedBody`)
-    )
-  })
+filter_ids.forEach(filter_id => buildReleaseNotes(filter_id));
 
-  .map(params => {
-    let issue = params[0];
-    let comments = JSON.parse(params[1].body).comments;
-    /* Find the comment we will display in release notes */
-    let rn_comment = comments.filter(comment => {
-      let search = comment.body.search('Description for release notes:');
-      if (search != -1) {
-        return comment;
-      }
-    });
-    return {
-      key: issue.key,
-      comment: rn_comment[0] ? rn_comment[0].renderedBody : '',
-      components: issue.fields.components
-    };
-  })
-  .subscribe({
-    onNext: data => {
-      result.issues.push(data);
-    },
-    onError: error => {
-      console.log(new Error(error));
-    },
-    onCompleted: () => {
-      /* Generate the release notes in the build directory */
-      let params = Object.assign(result, {query: known_issues});
-      fs.readFile('template.html', 'utf8', function (err, data) {
-        if (err) {
-          return console.log(err);
+function buildReleaseNotes(filter_id) {
+  let result = {issues: []};
+  /* Get the filter metadata */
+  requestRx.get(`${url}/rest/api/2/filter/${filter_id}`)
+    /* Query the filter */
+    .flatMap(data => {
+      let filter = JSON.parse(data.body);
+      let options = Object.assign({
+        body: {
+          "jql": filter.jql,
+          "fields": ["summary", "components", "comment"] /* Fields we need for release notes */
+        },
+        url: `${url}/rest/api/2/search?expand=renderedBody`
+      }, post_body);
+      return requestRx.post(options);
+    })
+    .flatMap(result => {
+      return rx.Observable.fromArray(result.body.issues);
+    })
+    /* Query each issue */
+    .flatMap(issue => {
+      return rx.Observable.zip(
+        rx.Observable.fromArray([issue]),
+        requestRx.get(`${url}/rest/api/2/issue/${issue.key}/comment?expand=renderedBody`)
+      )
+    })
+    .map(params => {
+      let issue = params[0];
+      let comments = JSON.parse(params[1].body).comments;
+      /* Find the comment we will display in release notes */
+      let rn_comment = comments.filter(comment => {
+        let search = comment.body.search('Description for release notes:');
+        if (search != -1) {
+          return comment;
         }
-        fs.writeFile('./build/release-notes.html', Mustache.render(data, params), function(err) {
-          if(err) {
+      });
+      return {
+        key: issue.key,
+        comment: rn_comment[0] ? rn_comment[0].renderedBody : '',
+        components: issue.fields.components
+      };
+    })
+    .subscribe({
+      onNext: data => {
+        result.issues.push(data);
+      },
+      onError: error => {
+        console.log(new Error(error));
+      },
+      onCompleted: (data) => {
+        /* Generate the release notes in the build directory */
+        let params = Object.assign(result, {filter: filter_id});
+        fs.copy('./img/', './build/img/', err => {
+          if (err) return console.error(err);
+        });
+        fs.readFile('template.html', 'utf8', function (err, data) {
+          if (err) {
             return console.log(err);
           }
-          console.log("The file was saved!");
+          fs.writeFile(`./build/release-notes-${filter_id}.html`, Mustache.render(data, params), function(err) {
+            if(err) {
+              return console.log(err);
+            }
+            console.log("The file was saved!");
+          });
         });
-      });
-      console.log('Completed')
-    }
-  });
+        console.log('Completed')
+      }
+    });
+}
